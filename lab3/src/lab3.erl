d1acbf3dacc033f592f7a045753d86213dc04a7c
олег <oleg.ru1234567890@mail.ru>
2023-01-27 12:47:14 +0300

    add delta input

--- lab3/src/lab3.erl
+++ lab3/src/lab3.erl
@@ -5,7 +5,10 @@
 start(_) ->
   Type_line = io:get_line("Type> "),
   Type_stripped_line = string:strip(string:strip(Type_line, both, 13), both, 10),
-  lab3_sup:start_link(0.25, Type_stripped_line),
+  Delta_line = io:get_line("Delta> "),
+  Delta_stripped_line = string:strip(string:strip(Delta_line, both, 13), both, 10),
+  {_, [Delta], _} = io_lib:fread("~f", Delta_stripped_line),
+  lab3_sup:start_link(Delta, Type_stripped_line),
   loop().
 
 loop() -> loop().
